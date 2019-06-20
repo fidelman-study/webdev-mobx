7694026f35e14eafce89f0bd8203ab609ef42da1
Andrei Fidelman <fidelmanlife@gmail.com>
2019-06-20 18:46:33 +0200

    split store

--- src/index.js
+++ src/index.js
@@ -5,25 +5,34 @@ import DevTools from 'mobx-react-devtools';
 import { observable } from 'mobx'
 import { observer } from 'mobx-react'
 
+const counterState = observable({
+  count: 0,
+})
+
+counterState.increment = function() {
+  this.count++
+}
+counterState.decrement = function() {
+  this.count--
+}
+
 @observer
 class Counter extends Component {
-  @observable count = 0
-
-  handleIncrement = () => this.count++
-  handleDecrement = () => this.count--
+  handleClickDecrement = () => this.props.store.decrement()
+  handleClickIncrement = () => this.props.store.increment()
 
   render() {
     return (
       <div className="App">
         <DevTools />
-        <h1>Count: {this.count}</h1>
-        <button onClick={this.handleIncrement}>+1</button>
-        <button onClick={this.handleDecrement}>-1</button>
+        <h1>Count: {this.props.store.count}</h1>
+        <button onClick={this.handleClickDecrement}>-1</button>
+        <button onClick={this.handleClickIncrement}>+1</button>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counter store={counterState} />, document.getElementById('root'));
 
 serviceWorker.unregister();
