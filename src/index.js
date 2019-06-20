2cce768b693f68754170de95ff9a98db8f34a497
Andrei Fidelman <fidelmanlife@gmail.com>
2019-06-20 18:41:58 +0200

    decorators observable observer

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import DevTools from 'mobx-react-devtools';
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
 
-class App extends Component {
+@observer
+class Counter extends Component {
+  @observable count = 0
+
+  handleIncrement = () => this.count++
+  handleDecrement = () => this.count--
+
   render() {
     return (
       <div className="App">
         <DevTools />
-        <h1>Hello world!</h1>
+        <h1>Count: {this.count}</h1>
+        <button onClick={this.handleIncrement}>+1</button>
+        <button onClick={this.handleDecrement}>-1</button>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
 
 serviceWorker.unregister();
